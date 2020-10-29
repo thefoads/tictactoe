5c8726fbe6384e4ea9f3314db15f8441436aa0e2
Ben Foad <bfoad@telxl.com>
2020-10-29 11:47:31 +0000

    Changed Winner to Victor

--- src/index.js
+++ src/index.js
@@ -143,7 +143,7 @@ function Square(props) {
 
       let status;
       if (winner){
-        status = 'Winner: ' + winner;
+        status = 'Victor: ' + winner;
       }
       else{
         status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
